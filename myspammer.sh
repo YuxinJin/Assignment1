f981f574ffedb4f704e16fe0536f1fd1665a1c57
Yuxin <35482833+YuxinJin@users.noreply.github.com>
2018-01-18 19:41:13 -0800

    Update myspammer.sh

--- myspammer.sh
+++ myspammer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo 'Dear Professor Craig, this is my assignment1' | mail -s 'TRGN510 Assign1' yuxinjin@usc.edu
+echo 'Dear Professor Craig, this is my assignment1' | mail -s 'TRGN510 Assign1' davidwcr@usc.edu
